45e0e66e17d3bc4e863b68b11d83f46e6b8e9559
wushan <1641892578@qq.com>
2017-12-04 20:15:24 +0800

    wushan

---
+++ src/main/java/com/ws/controller/Text.java
@@ -0,0 +1,13 @@
+package com.ws.controller;
+
+/**
+ * @author
+ * @create 2017-12-04
+ * @desc 这是一个类
+ **/
+public class Text {
+
+    public static void main(String[] args) {
+        System.out.println("sss");
+    }
+}
